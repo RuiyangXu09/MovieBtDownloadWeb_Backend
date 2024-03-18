62bf251065a8be59566f56299e95e9b8f663ac5b
Ruiyang Xu <332971895@qq.com>
2024-03-18 18:11:48 +0800

    Configure the Mybatis mapper, including the XML mapping file for the movieBtMapper interface and the corresponding SQL statements

--- src/main/java/com/example/moviedownloadbtweb/mapper/MovieBtMapper.java
+++ src/main/java/com/example/moviedownloadbtweb/mapper/MovieBtMapper.java
@@ -6,7 +6,7 @@ import org.apache.ibatis.annotations.*;
 import java.util.List;
 
 /**
- * 电影管理
+ * 电影管理的sql方法，映射文件名：MovieBtMapper.xml
  */
 @Mapper
 public interface MovieBtMapper {
@@ -14,30 +14,25 @@ public interface MovieBtMapper {
      * 新增电影bt信息的sql
      * @param movie
      */
-    @Insert("INSERT INTO movie(movie_name, category, create_time, update_time, bt_download_url, subtitle_download_url, count_download) " +
-            "VALUES (#{movieName}, #{category}, #{createTime}, #{updateTime}, #{btDownloadUrl}, #{subtitleDownloadUrl}, 1)")
     void addMovie(MovieBt movie);
 
     /**
-     * 修改电影信息的sql
+     * 根据id修改电影信息的sql
      * @param movieBt
      */
-    @Update("UPDATE movie SET movie_name = #{movieName}, category = #{category}, update_time = #{updateTime}, bt_download_url = #{btDownloadUrl}, subtitle_download_url = #{subtitleDownloadUrl} " +
-            "WHERE id = #{id}")
     void updateMovie(MovieBt movieBt);
 
     /**
      * 删除电影信息的sql，by id
      * @param id
      */
-    @Delete("DELETE FROM movie WHERE id = #{id}")
+//    @Delete("DELETE FROM movie WHERE id = #{id}")
     void deleteMovie(Integer id);
 
     /**
      * 根据点击次数列出电影信息，由大到小的sql
      * @return
      */
-    @Select("SELECT id, movie_name, category, create_time, update_time, bt_download_url, subtitle_download_url FROM movie ORDER BY count_download DESC")
     List<MovieBt> listMovieByClickNumber();
 
     /**
@@ -46,13 +41,11 @@ public interface MovieBtMapper {
      * 利用了数据库的事务性和原子性来确保每次点击都被正确记录，并且即使在并发环境下也能保持数据的一致性
      * 同时，数据也被持久化保存在数据库中，不会因为服务重启而丢失
      */
-    @Update("UPDATE movie SET count_download = count_download + 1 WHERE bt_download_url = #{btDownloadUrl}")
     void countAddOne(String btDownloadUrl);
 
     /**
-     * 查询数据库中，电影的总记录数
+     * 查询数据库中，电影的总记录数的sql
      */
-    @Select("SELECT count(*) FROM movie")
     Integer countMovie();
 
     /**
@@ -61,6 +54,5 @@ public interface MovieBtMapper {
      * @param pageSize
      * @return
      */
-    @Select("SELECT id, movie_name, category, update_time, bt_download_url, subtitle_download_url, count_download FROM movie LIMIT #{indexStart}, #{pageSize}")
     List<MovieBt> pageMovie(Integer indexStart, Integer pageSize);
 }
